d9f17b62e3f99dd5582ec8f7d67ca2c60b8d7980
Stavros Kefaleas <stavros.kefaleas@gmail.com>
2019-07-23 21:09:19 +0300

    Updates

---
+++ precache-manifest.32893c3d5db4064bf434c0848611298e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b5d037df5d76da929286cba7c862da7",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "e7ddd2cb38ac3619fb15",
+    "url": "/monstersRolodex/static/css/main.4f290134.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monstersRolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e7ddd2cb38ac3619fb15",
+    "url": "/monstersRolodex/static/js/main.4fcb835e.chunk.js"
+  },
+  {
+    "revision": "adbf709dcf00b1922f72",
+    "url": "/monstersRolodex/static/js/runtime~main.15a48f79.js"
+  }
+]);
